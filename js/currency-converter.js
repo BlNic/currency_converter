9fe7459b58ac37e7844dc4e257ab4e1fdbe5a3de
Blanka Nicman <bjnicman@o2.pl>
2022-12-29 15:00:12 +0100

    converter upgrade using select

--- js/currency-converter.js
+++ js/currency-converter.js
@@ -1,19 +1,36 @@
-let converter = document.querySelector(".converterForm");
-let quantityField = document.querySelector(".jsQuantity");
-let usd = document.querySelector(".currency__usd");
-let resultField = document.querySelector(".jsResult");
+let form = document.querySelector(".converterForm");
+let jsQuantity = document.querySelector(".jsQuantity");
+let jsRate = document.querySelector(".jsRate");
+let jsResult = document.querySelector(".jsResult");
+let currencyField = document.querySelector(".currencyField");
+let usd = document.querySelector(".jsRate__usd");
+let eur = document.querySelector(".jsRate__eur");
+let gbp = document.querySelector(".jsRate__gbp");
 
-converter.addEventListener("input"), (event) => {
+form.addEventListener("input", (event) => {
     event.preventDefault();
 
-    let quantity = quantityField.value;
-    let result = quantity * usd;
-    jsResult.innerTextresult.toFixed(2)
+    let number = jsQuantity.value;
+    let currency = jsRate.value;
+    let result = number * currency;
 
-}
+    if (jsRate.value === usd.value) {
+        currencyField.innerText = "USD"
+    }
+    else if (jsRate.value === eur.value) {
+        currencyField.innerText = "EUR"
+    }
+    else if (jsRate.value === gbp.value) {
+        currencyField.innerText = "GBP"
+    }
 
+    jsResult.innerText = result.toFixed(2);
+});
 
-
-
-
-
+form.addEventListener("reset", () => {
+    jsResult.innerText = "0.00";
+});
+/*
+form.addEventListener("reset", () => {
+  currencyField.innerText = " ";
+}); */
